088547606e6753997b7e1f05dba88e6904a8a45e
atrox3d <atrox3d@gmail.com>
2023-01-09 23:10:40 +0100

    89. Resize Image

--- resize-image.py
+++ resize-image.py
@@ -1,22 +1,36 @@
 import cv2
+from pathlib import Path
 
 
 def calculate_size(scale_percentage, width, height):
     w = width * scale_percentage / 100
     h = height * scale_percentage / 100
-    return int(w), int(h)
+    return int(h), int(w)
 
 
-def resize(image_path, scale_percentage, resized_path):
+def resize(image_path, scale_percentage, resized_path=None):
     image = cv2.imread(image_path)
+
     print(f'{image.shape=}')
     height, width, _ = image.shape
-    dsize = calculate_size(scale_percentage, width, height)
+
+    h, w = calculate_size(scale_percentage, width, height)
+    dsize = w, h
     print(f'{dsize=}')
+
     resized = cv2.resize(image, dsize)
     print(f'{resized.shape=}')
+
+    if not resized_path:
+        path = Path(image_path)
+        height, width, _ = resized.shape
+        dims = f'{height}x{width}'
+        filename = f'{path.stem}-{dims}{path.suffix}'
+        resized_path = Path('resized-images', filename).__str__()
+
+    print(resized_path)
     cv2.imwrite(resized_path, resized)
 
 
-resize('images/galaxy.jpeg', 20, 'images/galaxy-resized.jpg')
+resize('images/galaxy.jpeg', 20)
 
